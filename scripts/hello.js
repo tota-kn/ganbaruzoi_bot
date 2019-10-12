ed090ff10f844e7ae1608c4e5f96bc37822855cc
yt <owner@ownernoiMac.local>
2019-10-12 17:42:00 +0900

    add:応答で画像を表示

--- scripts/hello.js
+++ scripts/hello.js
@@ -3,5 +3,7 @@ exports.__esModule = true;
 module.exports = function (robot) {
     robot.respond(/今日も一日/i, function (msg) {
         msg.reply("がんばるぞい!");
+        var imageUrl = "https://gyazo.com/8e9f54cdb2b5b9ad4d646c49ee62c5eb";
+        msg.reply(imageUrl);
     });
 };
--- scripts/hello.ts
+++ scripts/hello.ts
@@ -3,5 +3,7 @@ import hubot = require("hubot");
 module.exports = (robot: hubot.Robot<any>): void => {
     robot.respond(/今日も一日/i, (msg: hubot.Response<any>) => {
         msg.reply("がんばるぞい!");
+        const imageUrl = "https://gyazo.com/8e9f54cdb2b5b9ad4d646c49ee62c5eb";
+        msg.reply(imageUrl);
     });
 };
